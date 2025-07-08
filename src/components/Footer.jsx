e8a447219862f4236ddbe40eabfb4c8977e967af
Zofia Nora <sophienora92@gmail.com>
2025-07-08 13:23:49 +0200

    added content for footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <>
+        <footer>
+            <div className="container">
+                <p>coded and designed by @sopheenori </p>
+            </div>
+        </footer>
+        </>
+    )
+}

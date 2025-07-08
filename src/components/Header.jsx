7da518559e58a72302ec6c5a03f406671ef4b6e8
Zofia Nora <sophienora92@gmail.com>
2025-07-08 12:36:33 +0200

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="header">
+        <div className="container">
+            <h1>learned()</h1>
+            <p>a daily learning journal to reflect on what you already learned</p>
+        </div>  
+    </header>
+  );
+}
+
+export default Header;

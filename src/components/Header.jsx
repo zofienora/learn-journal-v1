1b4b617a190f3e6c4dd799ed7d2c5594f6177cee
Zofia Nora <sophienora92@gmail.com>
2025-07-08 13:27:28 +0200

    added style to header appname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="header">
         <div className="container">
-            <h1>learned()</h1>
+            <h1 className="appname">learned()</h1>
             <p>a daily learning journal to reflect on what you already learned</p>
         </div>  
     </header>

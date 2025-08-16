894a2c91ea31f6342059342ee9e97dd656fc4e00
Zofia Nora <sophienora92@gmail.com>
2025-08-16 18:48:50 +0200

    added first block for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ function Header() {
   return (
     <header className="header">
         <div className="container">
-            <h1 className="appname">The Daily Learn Journal</h1>
-            <p className="first_sentence">What did I learn today?</p>
+            <div className="header-left-one">
+                <p className="header-one">LEARNED()</p>
+                <p className="header-one">VOLUME <span className="span-red">1</span> — ENTRY LOG</p>
+                <p className="header-two">a minimal learning journal</p>
+            </div>
         </div>  
        {/*  <div className="container">
             <button className="darkToggle">Light/Dark</button>

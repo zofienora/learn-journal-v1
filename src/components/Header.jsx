8e1aa3a5f041eb8b236df280ff4bd35d58ad4d32
Zofia Nora <sophienora92@gmail.com>
2025-08-16 18:55:57 +0200

    added content for first part of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,16 @@ function Header() {
   return (
     <header className="header">
         <div className="container">
-            <div className="header-left-one">
-                <p className="header-one">LEARNED()</p>
-                <p className="header-one">VOLUME <span className="span-red">1</span> — ENTRY LOG</p>
-                <p className="header-two">a minimal learning journal</p>
-            </div>
+          <div className="header-above">
+              <div className="header-left-one">
+                  <p className="header-one">LEARNED()</p>
+                  <p className="header-one">VOLUME <span className="span-red">1</span> — ENTRY LOG</p>
+                  <p className="header-two">a minimal learning journal</p>
+              </div>
+              <div className="header-right-one">
+                <p className="header-three">Learning doesn’t happen all at once — it builds, moment by moment. Recording your reflections transforms small steps into lasting insight.</p>
+              </div>
+          </div>
         </div>  
        {/*  <div className="container">
             <button className="darkToggle">Light/Dark</button>

93c003bab33e1c698a60652851832ffbb537b05e
Zofia Nora <sophienora92@gmail.com>
2025-08-16 19:40:00 +0200

    added some css changed in first part header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
         <div className="container">
           <div className="header-above">
               <div className="header-left-one">
-                  <p className="header-one">LEARNED()</p>
+                  <p className="header-one-one">LEARNED()</p>
                   <p className="header-one">VOLUME <span className="span-red">1</span> — ENTRY LOG</p>
                   <p className="header-two">a minimal learning journal</p>
               </div>

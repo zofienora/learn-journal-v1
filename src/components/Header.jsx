7b02643fdf1abcb764abea1a09b1072d370767b1
Zofia Nora <sophienora92@gmail.com>
2025-08-18 16:20:51 +0200

    added current date to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
 function Header() {
+  const today = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+
+
   return (
     <header className="header">
         <div className="container">
           <div className="header-above">
               <div className="header-left-one">
-                  <p className="header-one-one">LEARNED()</p>
                   <p className="header-one">VOLUME <span className="span-red">1</span> — ENTRY LOG</p>
                   <p className="header-two">a minimal learning journal</p>
               </div>
               <div className="header-right-one">
                 <p className="header-three">Learning doesn’t happen all at once — it builds, moment by moment. Recording your reflections transforms small steps into lasting insight.</p>
               </div>
+          </div>
+          <div className="header-below">
+            <div className="below-one">{today}</div>
+            <div className="below-two"></div>
           </div>
         </div>  
        {/*  <div className="container">
